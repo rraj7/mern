229cdb45f472635c6cc81636351c88314b3f555d
Rishi Raj <rraj7@uic.edu>
2020-05-25 03:27:11 -0500

    Added the APIs

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,12 @@ require('dotenv').config();
 
 var app = express();
 
+//MIDDLEWARE
 app.use(cors());
 app.use(express.json());
 
+
+//CONNECT DB
 var uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
 var connection = mongoose.connection;
@@ -17,5 +20,12 @@ connection.once('open', ()=> {
     console.log('MongoDB connected');
 })
 
+
+//Routes Folder
+var exerciseRouter = require('./routes/exercises');
+var userRouter = require('./routes/users');
+
+app.use('/exercises',exerciseRouter);
+app.use('/users',userRouter); 
 app.listen(3000);
  
