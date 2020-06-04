01f40f4bc4e2418bacd646ce6e594a9f585871a2
Rishi Raj <rraj7@uic.edu>
2020-06-04 01:54:53 -0500

    Added Routes

--- mern-project/src/App.js
+++ mern-project/src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 
-function App() {
+import Navbar from "./components/navbar.component";
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
+
+ //import logo from './img.svg';
+//import './App.css';
+
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -23,4 +32,19 @@ function App() {
   );
 }
 
+export default App;
+*/
+
+function App(){
+  return(
+    <Router>
+      <Navbar />
+      <br/>
+      <Route path="/" exact component={ExercisesList} />
+      <Route path="/edit/:id"  component={EditExercise} />
+      <Route path="/create"  component={CreateExercise} />
+      <Route path="/user"  component={CreateUser} />
+    </Router>
+  );
+}
 export default App;
